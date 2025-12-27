bd1c754239f2d0c491be5b9edee0a27802beed76
Centrix14 <artiom.zvyagin@yandex.ru>
2025-12-27 12:50:31 +0300

    Added timestumps at result file name

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 cd $APP_PATH
 ./gradlew --no-daemon assembleDebug
 cp /app/build/outputs/apk/debug/juloo.keyboard2.debug.apk \
-   /dockapk-output/unexpected-keyboard.apk
+   /dockapk-output/unexpected-keyboard-`date +%s`.apk
